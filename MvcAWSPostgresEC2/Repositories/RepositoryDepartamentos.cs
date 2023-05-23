3604b7f3185e44ebd8379733dcb2450cba56ad53
Carlos00-pixel <58859997+Carlos00-pixel@users.noreply.github.com>
2023-05-23 12:49:42 +0200

    Commit

---
+++ MvcAWSPostgresEC2/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcAWSPostgresEC2.Data;
+using MvcAWSPostgresEC2.Models;
+
+namespace MvcAWSPostgresEC2.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int id)
+        {
+            return await this.context.Departamentos
+                .FirstOrDefaultAsync(x => x.IdDepartamento == id);
+        }
+
+        public async Task InsertDepartamentoAsync
+            (int id, string nombre, string localidad)
+        {
+            Departamento dept = new Departamento();
+            dept.IdDepartamento = id;
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            this.context.Departamentos.Add(dept);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
